5a87e4ef0feb5ee98f60a74976e74ccbd2da8f8d
MichałM <michalhm@gmail.com>
2023-05-29 20:31:43 +0200

    Dodano wartości inflacyjne

--- main.py
+++ main.py
@@ -38,5 +38,32 @@ while poprawnosc_danych == False:
     else:
         poprawnosc_danych = False
 
+# inflacja_rok_miesiac
+
+inflacja_1_styczen = 1.592824484
+inflacja_1_luty = -0.453509101
+inflacja_1_marzec = 2.324671717
+inflacja_1_kwiecien = 1.261254407
+inflacja_1_maj = 1.782526286
+inflacja_1_czerwiec = 2.329384541
+inflacja_1_lipiec = 1.502229842
+inflacja_1_sierpien = 1.782526286
+inflacja_1_wrzesien = 2.328848994
+inflacja_1_pazdziernik = 0.616921348
+inflacja_1_listopad = 2, 352295886
+inflacja_1_grudzien = 0.337779545
+inflacja_2_styczen = 1.577035247
+inflacja_2_luty = -0.292781443
+inflacja_2_marzec = 2.48619659
+inflacja_2_kwiecien = 0.267110318
+inflacja_2_maj = 1.417952672
+inflacja_2_czerwiec = 1.054243267
+inflacja_2_lipiec = 1.480520104
+inflacja_2_sierpien = 1.577035247
+inflacja_2_wrzesien = -0.07742069
+inflacja_2_pazdziernik = 1.165733399
+inflacja_2_listopad = -0.404186718
+inflacja_2_grudzien = 1.499708521
+
 
 print("Koniec programu")
